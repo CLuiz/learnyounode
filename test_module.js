054ed4d1f7c0b79a233262e153c5a250baa0e68b
Chris Luiz <chris@chrisluiz.io>
2017-04-13 07:26:08 -0600

    minor changes

---
+++ test_module.js
@@ -0,0 +1,22 @@
+// prints list of files in a given directory filtered by file extension
+var fs = require('fs');
+
+directory = process.argv[2];
+extension = process.argv[3];
+
+fs.readdir(directory, doStuff);
+
+function wrapItUp()
+function doStuff(err, files) {
+      if (err) {
+          console.log(err);
+      } else {
+          for (var i=0; i<files.length; i++) {
+              //console.log(files[i]);
+              if (files[i].split('.')[1] == extension) {
+                  console.log(files[i]);
+              }
+          }
+      }
+
+}
